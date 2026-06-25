31f95845a1ec804005c084844212ddf6fd68f7d2
slehmadi <ishlahims@gmail.com>
2026-06-25 11:34:39 +0700

    (frontend) Creating route/ProtectedRoute.jsx

---
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+
+import { AuthContext } from '../contexts/AuthContext';
+
+function ProtectedRoute({ children }) {
+    const { user } = useContext(AuthContext);
+
+    if (!user) {
+        return <Navigate to='/login' replace />;
+    }
+
+    return children;
+}
+
+export default ProtectedRoute;
