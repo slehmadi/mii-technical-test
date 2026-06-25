4ca586df845ba652ad1f3ec0caa8ae3c6064394e
slehmadi <ishlahims@gmail.com>
2026-06-25 17:00:29 +0700

    (frontend) Update ProtectedRoute.jsx - adding location to save web endpoint state before accessing login

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,13 +1,14 @@
 import { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 import { AuthContext } from '../contexts/AuthContext';
 
 function ProtectedRoute({ children }) {
     const { user } = useContext(AuthContext);
+    const location = useLocation();
 
     if (!user) {
-        return <Navigate to='/login' replace />;
+        return <Navigate to='/login' state={{ from: location }} replace />;
     }
 
     return children;
