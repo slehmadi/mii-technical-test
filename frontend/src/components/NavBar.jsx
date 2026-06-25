18fc73451bdec527d64aefa6f6186c4cbad22239
slehmadi <ishlahims@gmail.com>
2026-06-25 09:56:53 +0700

    (frontend) Updating Navbar component

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,35 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { 
+    AppBar, 
+    Toolbar, 
+    Typography,
+    Button,
+    Box
+} from '@mui/material';
+
+import { Link } from 'react-router-dom'
 
 function NavBar() {
     return (
         <AppBar position='static'>
             <Toolbar>
-                <Typography>
+                <Typography variant='h6' sx={{ flexGrow: 1 }}>
                     E-Commerce Observability
                 </Typography>
+
+                <Box>
+                    <Button color='inherit' component={Link} to='/'>
+                        Home
+                    </Button>
+                    <Button color='inherit' component={Link} to='/cart'>
+                        Cart
+                    </Button>
+                    <Button color='inherit' component={Link} to='/profile'>
+                        Profile
+                    </Button>
+                    <Button color='inherit' component={Link} to='/login'>
+                        Login
+                    </Button>
+                </Box>
             </Toolbar>
         </AppBar>
     );
