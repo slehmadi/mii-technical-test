cbff228a6861249d79721dfaf3b6aa34e389444e
slehmadi <ishlahims@gmail.com>
2026-06-25 08:55:19 +0700

    Initial Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Container, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container sx={{ mt: 5 }}>
+      <Typography variant="h3">
+        E-Commerce Observability App
+      </Typography>
+
+      <Typography variant="body1">
+        Frontend setup successful.
+      </Typography>
+    </Container>
+  );
+}
+
+export default App;
