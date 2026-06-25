18d8a5d5a171bc8c6b068f1fb10180db40483710
slehmadi <ishlahims@gmail.com>
2026-06-25 10:55:19 +0700

    (frontend) Updating HomePage.jsx and Fixing ProductCard.jsx component (adding return)

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -10,32 +10,38 @@ import {
 import { Link } from 'react-router-dom';
 
 function ProductCard({ product }) {
-    <Card>
-        <CardMedia 
-            component='img' 
-            height="220" 
-            image={
-                product.image_url || "https://placehold.co/300x220"
-            } 
-        />
-        <CardContent>
-            <Typography variant='h6'>
-                {product.name}
-            </Typography>
-            <Typography variant='body2'>
-                {product.description}
-            </Typography>
-            <Typography variant='h6' sx={{ mt: 2 }}>
-                Rp {product.price.toLocaleString()}
-            </Typography>
-        </CardContent>
-        <CardActions>
-            <Button component={Link} to={`/products/${product.id}`} >
-                Detail
-            </Button>
-            <Button variant='contained'>
-                Add to Cart
-            </Button>
-        </CardActions>
-    </Card>
+
+    return(
+        <Card>
+            <CardMedia 
+                component='img' 
+                height="220" 
+                image={
+                    product.image_url || "https://placehold.co/300x220"
+                } 
+            />
+            <CardContent>
+                <Typography variant='h6'>
+                    {product.name}
+                </Typography>
+                <Typography variant='body2'>
+                    {product.description}
+                </Typography>
+                <Typography variant='h6' sx={{ mt: 2 }}>
+                    Rp {product.price.toLocaleString()}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button component={Link} to={`/products/${product.id}`} >
+                    Detail
+                </Button>
+                <Button variant='contained'>
+                    Add to Cart
+                </Button>
+            </CardActions>
+        </Card>
+    )
+
 }
+
+export default ProductCard;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,29 @@
-import { Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material';
+
+import ProductCard from '../components/ProductCard'
+import products from '../data/products.json'
 
 function HomePage() {
-    return <Typography variant='h4'>Home Page</Typography>
+    return (
+        <>
+            <Typography variant='h4' gutterBottom>
+                Product Catalog
+            </Typography>
+
+            <Grid container spacing={3}>
+
+                {products.map((product) => (
+                    <Grid 
+                        size={{ xs: 12, sm: 6, md: 4 }}
+                        key={product.id}
+                    >
+                        <ProductCard product={product} />
+                    </Grid>
+                ))}
+
+            </Grid>
+        </>
+    )
 }
 
 export default HomePage;
