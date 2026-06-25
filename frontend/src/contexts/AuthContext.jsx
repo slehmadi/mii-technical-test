0d950770c5de52a8ddca140d5f2e7ff0fb2b201a
slehmadi <ishlahims@gmail.com>
2026-06-25 11:40:15 +0700

    (frontend) Update AuthContext.jsx - Adding savedUser, login, and logout info

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,15 +1,39 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
 
+    useEffect(() => {
+        const savedUser = localStorage.getItem("user");
+
+        if (savedUser) {
+            setUser(JSON.parse(savedUser));
+        }
+    }, []);
+
+    const login = (userData) => {
+        setUser(userData);
+
+        localStorage.setItem(
+            "user",
+            JSON.stringify(userData)
+        );
+    };
+
+    const logout = () => {
+        setUser(null);
+
+        localStorage.removeItem("user");
+    }
+
     return (
         <AuthContext.Provider 
             value={{
                 user,
-                setUser
+                login,
+                logout
             }}
         >
             {children}
