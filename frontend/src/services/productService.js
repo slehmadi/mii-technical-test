6e4a5da46cd6180feb2cac05523366eca71cab41
slehmadi <ishlahims@gmail.com>
2026-06-25 17:53:51 +0700

    (frontend) Edit productService.js to compatible with backend

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,5 +1,20 @@
 import products from '../data/products.json'
 
-export const getProducts = async() => {
-    return products
+const productServices = {
+
+    // Get all products
+    getProducts: async() => {
+        return Promise.resolve(products)
+    },
+
+    // Get product by ID
+    getProductById: async(id) => {
+        const product = products.find(
+            (item) => item.id === Number(id)
+        );
+
+        return Promise.resolve(product)
+    }
 }
+
+export default productServices;
