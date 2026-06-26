ef251bbf49168563aa6f0a7d6fe48b0b6eefb4f6
slehmadi <ishlahims@gmail.com>
2026-06-26 21:41:13 +0700

    (backend-auth) Modifying app/main.py - Implement DB on startup

--- backend/auth-service/app/main.py
+++ backend/auth-service/app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 
+from app.db.database import engine
+from app.db.models import Base
+
 app = FastAPI(title="Auth Service")
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/health")
 def health_check():
