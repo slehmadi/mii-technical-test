e9a80ac66740ad45b1bcd7e585480c7019fd57fb
slehmadi <ishlahims@gmail.com>
2026-06-26 22:21:47 +0700

    (backend-auth) Updating app/main.py - Including router (app/api/routes.py)

--- backend/auth-service/app/main.py
+++ backend/auth-service/app/main.py
@@ -3,11 +3,15 @@ from fastapi import FastAPI
 from app.db.database import engine
 from app.db.models import Base
 
+from app.api.routes import router
+
 app = FastAPI(title="Auth Service")
 
 @app.on_event("startup")
 def startup():
     Base.metadata.create_all(bind=engine)
+
+app.include_router(router)
 
 @app.get("/health")
 def health_check():
