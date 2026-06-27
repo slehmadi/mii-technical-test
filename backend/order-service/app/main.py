f5fa38d361fa98866594461d975832c0fef0c74c
slehmadi <ishlahims@gmail.com>
2026-06-27 23:07:27 +0700

    (backend) Updating every app/main.py - Testing if testing step on pipeline will failed

--- backend/order-service/app/main.py
+++ backend/order-service/app/main.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
 from app.core.config import settings
+from app.db.database import Base, engine
 
-app = FastAPI(title="Order Service")
+app = FastAPI(title="Auth Service")
+
+@app.on_event('startup')
+def startup():
+    Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
--- backend/product-service/app/main.py
+++ backend/product-service/app/main.py
@@ -3,8 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import router
 from app.core.config import settings
+from app.db.database import Base, engine
 
-app = FastAPI(title="Product Service")
+app = FastAPI(title="Auth Service")
+
+@app.on_event('startup')
+def startup():
+    Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
