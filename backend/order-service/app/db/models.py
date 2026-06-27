7db5133df92c09b82c6bad6694a043f83e2749de
slehmadi <ishlahims@gmail.com>
2026-06-27 16:34:34 +0700

    (backend-order) Initial app/db/models.py

---
+++ backend/order-service/app/db/models.py
@@ -0,0 +1,37 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    Float,
+    String,
+    ForeignKey,
+    DateTime
+)
+
+from sqlalchemy.orm import relationship
+
+from datetime import datetime, UTC
+
+from app.db.database import Base
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, nullable=False)
+    status = Column(String(50), default="Pending")
+    total_price = Column(Float, nullable=False)
+    created_at = Column(DateTime(timezone=True), default=datetime.now(UTC))
+    items = relationship("OrderItem", back_populates="order", cascade="all, delete")
+
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer, ForeignKey("orders.id"))
+    product_id = Column(Integer)
+    product_name = Column(String(255))
+    quantity = Column(Integer)
+    price = Column(Float)
+    order = relationship("Order", back_populates="items")
