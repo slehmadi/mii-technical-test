7db005c659db0fbf153cbf52172696014ea0ac37
slehmadi <ishlahims@gmail.com>
2026-06-27 10:45:27 +0700

    (backend-auth) Update app/core/security.py - Adding verify_access_token function

--- backend/auth-service/app/core/security.py
+++ backend/auth-service/app/core/security.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, timezone
 
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 
 from app.core.config import settings
@@ -23,3 +23,10 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
 
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        return payload
+    except JWTError:
+        return None
