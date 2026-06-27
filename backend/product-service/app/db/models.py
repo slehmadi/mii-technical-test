b443e69357b0e7b7ff5fab3bc7a0507b206c37af
slehmadi <ishlahims@gmail.com>
2026-06-27 14:36:41 +0700

    (backend-product) Adding initial app/db/models.py

---
+++ backend/product-service/app/db/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float, Text
+from app.db.database import Base
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    description = Column(Text, nullable=True)
+    price = Column(Float, nullable=False)
+    image_url = Column(String(500))
+    category = Column(String(100))
