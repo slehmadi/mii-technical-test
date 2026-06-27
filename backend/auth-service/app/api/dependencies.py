3f7117d491265b0aef05a6dde37afa17bee9418e
slehmadi <ishlahims@gmail.com>
2026-06-27 11:25:37 +0700

    (backend-auth) Modifying app/api/dependencies.py - Change security type

--- backend/auth-service/app/api/dependencies.py
+++ backend/auth-service/app/api/dependencies.py
@@ -1,14 +1,19 @@
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 
 from app.db.database import get_db
 from app.db.models import User
 from app.core.security import verify_access_token
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+security = HTTPBearer()
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+def get_current_user(
+        credentials: HTTPAuthorizationCredentials = Depends(security), 
+        db: Session = Depends(get_db)
+    ):
+    
+    token = credentials.credentials
     payload = verify_access_token(token)
 
     if not payload:
