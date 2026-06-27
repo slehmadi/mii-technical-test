359d473ef3ebab56ca7cf3d8c4a59e09a4acff1c
slehmadi <ishlahims@gmail.com>
2026-06-27 20:29:52 +0700

    (backend-product) Adding backend CORS origin - test#1

--- backend/product-service/app/main.py
+++ backend/product-service/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import router
+from app.core.config import settings
 
 app = FastAPI(title="Product Service")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(router)
 
