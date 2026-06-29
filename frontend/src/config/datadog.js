c9cc2601f62c7a774f0f7bbd5d9c67f2343557e6
slehmadi <ishlahims@gmail.com>
2026-06-29 10:16:20 +0700

    (frontend) Modifying src/config/datadog.js

--- frontend/src/config/datadog.js
+++ frontend/src/config/datadog.js
@@ -1,16 +1,11 @@
 import { datadogRum } from '@datadog/browser-rum';
 
-console.log("DD APP ID:", import.meta.env.VITE_DD_APP_ID);
-console.log("DD TOKEN:", import.meta.env.VITE_DD_CLIENT_TOKEN);
-console.log("DD SITE:", import.meta.env.VITE_DD_SITE);
-console.log(import.meta.env);
-
 datadogRum.init({
     applicationId: import.meta.env.VITE_DD_APP_ID,
     clientToken: import.meta.env.VITE_DD_CLIENT_TOKEN,
     site: import.meta.env.VITE_DD_SITE,
     service: 'frontend',
-    env: 'dev',				// e.g. 'prod', 'staging-1', 'dev'
+    env: import.meta.env.MODE,				// e.g. 'prod', 'staging-1', 'dev'
     version: '1.0.0',	// e.g. '1.0.0'
     sessionSampleRate: 100,			// capture 100% of sessions
     sessionReplaySampleRate: 20,	// capture 20% of sessions with replay
