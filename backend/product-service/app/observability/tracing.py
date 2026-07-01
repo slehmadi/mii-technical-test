ea295ae759fadf03b88500c40559a31ec5f76614
slehmadi <ishlahims@gmail.com>
2026-07-01 13:13:37 +0700

    (backend) Updating app/observability/tracing.py - Another linting stuff

--- backend/product-service/app/observability/tracing.py
+++ backend/product-service/app/observability/tracing.py
@@ -3,10 +3,8 @@ from opentelemetry import trace
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 
-from opentelemetry.sdk.trace.export import (
-    BatchSpanProcessor,
-    OTLPSpanExporter
-)
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
